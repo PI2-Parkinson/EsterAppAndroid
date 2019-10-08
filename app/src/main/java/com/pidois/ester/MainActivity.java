fcc5a6774c232dce2b38d78681cfe16e2bdc3987
Vinicius Borges <vbda123@gmail.com>
2019-10-07 21:53:26 -0300

    Login cycle adjusted to the profile layout

---
+++ app/src/main/java/com/pidois/ester/MainActivity.java
@@ -0,0 +1,42 @@
+package com.pidois.ester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Button listeners
+        //findViewById(R.id.btn_conectar).setOnClickListener(this);
+        //findViewById(R.id.btn_exerc).setOnClickListener(this);
+        findViewById(R.id.btn_perfil).setOnClickListener(this);
+        //findViewById(R.id.btn_dicas).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.btn_conectar){
+            //todo
+        } else if (i == R.id.btn_exerc){
+            //todo
+        } else if (i == R.id.btn_perfil){
+            switchScreen(ProfileActivity.class);
+        } else if (i==R.id.btn_dicas){
+            //todo
+        }
+    }
+
+    private void switchScreen (Class cl){
+        Intent intent = new Intent(MainActivity.this, cl);
+        MainActivity.this.startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/pidois/ester/ProfileActivity.java
@@ -0,0 +1,81 @@
+package com.pidois.ester;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.btn_logout2).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }*/
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.btn_logout2){
+            signOut();
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user == null) {
+            findViewById(R.id.btn_logout2).setVisibility(View.INVISIBLE);
+        } else {
+            findViewById(R.id.btn_logout2).setVisibility(View.VISIBLE);
+        }
+    }
+}
