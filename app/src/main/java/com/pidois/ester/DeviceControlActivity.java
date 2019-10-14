a8da2966cd54a19a24d2ea3b5c62ad0bcf2adb7d
Isaac <isaacbmota@gmail.com>
2019-10-14 12:09:07 -0300

    Scan and display ble devices #19

---
+++ app/src/main/java/com/pidois/ester/DeviceControlActivity.java
@@ -0,0 +1,13 @@
+package com.pidois.ester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeviceControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
