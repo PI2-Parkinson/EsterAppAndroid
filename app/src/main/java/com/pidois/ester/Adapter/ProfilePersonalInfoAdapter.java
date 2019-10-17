90f9ba2108a53bb9b5024e346831d2105d466e00
Vinicius Borges <vbda123@gmail.com>
2019-10-17 16:35:09 -0300

    Viewholder of profile personal info

---
+++ app/src/main/java/com/pidois/ester/Adapter/ProfilePersonalInfoAdapter.java
@@ -0,0 +1,13 @@
+package com.pidois.ester.Adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pidois.ester.Adapter.ViewHolder.ProfilePersonalInfoHolder;
+
+//public class ProfilePersonalInfoAdapter extends RecyclerView.Adapter<ProfilePersonalInfoHolder> {
+
+
+//}
