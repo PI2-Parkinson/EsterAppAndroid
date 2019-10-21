f81e4b32bd9561151f608fc03be2c7b9875e5e09
ViBorges <vbda123@gmail.com>
2019-10-21 14:17:38 -0300

    preparing strap model to show its data on the profile page

--- app/src/main/java/com/pidois/ester/Models/Strap.java
+++ app/src/main/java/com/pidois/ester/Models/Strap.java
@@ -1,4 +1,38 @@
 package com.pidois.ester.Models;
 
 public class Strap {
+
+    int tremorPos1;
+    int tremorPos2;
+    int tremorPos3;
+
+    public Strap (int tremorPos1, int tremorPos2, int tremorPos3){
+        this.tremorPos1 = tremorPos1;
+        this.tremorPos2 = tremorPos2;
+        this.tremorPos3 = tremorPos3;
+    }
+
+    public int getTremorPos1() {
+        return tremorPos1;
+    }
+
+    public int getTremorPos2() {
+        return tremorPos2;
+    }
+
+    public int getTremorPos3() {
+        return tremorPos3;
+    }
+
+    public void setTremorPos1(int tremorPos1) {
+        this.tremorPos1 = tremorPos1;
+    }
+
+    public void setTremorPos2(int tremorPos2) {
+        this.tremorPos2 = tremorPos2;
+    }
+
+    public void setTremorPos3(int tremorPos3) {
+        this.tremorPos3 = tremorPos3;
+    }
 }
