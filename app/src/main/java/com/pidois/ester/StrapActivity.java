fc9752e9ab2001a74cf07279c1c4a941e7c86c7b
Cecilia <ceciliabessa@hotmail.com>
2019-10-25 22:13:23 -0300

    Creating strap layout

---
+++ app/src/main/java/com/pidois/ester/StrapActivity.java
@@ -0,0 +1,64 @@
+package com.pidois.ester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class StrapActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_strap);
+
+        // Button listeners
+        findViewById(R.id.btn_m1).setOnClickListener(this);
+        findViewById(R.id.btn_m2).setOnClickListener(this);
+        findViewById(R.id.btn_m3).setOnClickListener(this);
+        findViewById(R.id.btn_m4).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.btn_m1){
+            //todo
+        } else if (i == R.id.btn_m2){
+            //todo
+        } else if (i == R.id.btn_m3){
+            //todo
+        } else if (i == R.id.btn_m4){
+            alertDialog();
+        }
+
+    }
+
+    private void alertDialog() {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage("Please Select any option");
+        dialog.setTitle("Dialog Box");
+        dialog.setPositiveButton("YES",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Toast.makeText(getApplicationContext(),"Yes is clicked",Toast.LENGTH_LONG).show();
+                    }
+                });
+        dialog.setNegativeButton("cancel",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"cancel is clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
+    }
+
+}
