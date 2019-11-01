7db9f9d1451473e579efcdacbc83d09534df9231
ViBorges <vbda123@gmail.com>
2019-11-01 18:27:22 -0300

    Adding chronometer to strap rest layout
    Co-Authored-By: CeciliaDib <ceciliabessa@hotmail.com>

--- app/src/main/java/com/pidois/ester/Controller/StrapRestActivity.java
+++ app/src/main/java/com/pidois/ester/Controller/StrapRestActivity.java
@@ -19,11 +19,39 @@ import com.pidois.ester.R;
 import java.util.Locale;
 
 public class StrapRestActivity extends AppCompatActivity {
-    
+
+    private Chronometer chronometer;
+    private Button button;
+    private int time = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_strap_rest);
+
+        chronometer = findViewById(R.id.strap_rest_chronometer);
+        button = findViewById(R.id.strap_rest_btn);
+
+        chronometer.setBase(SystemClock.elapsedRealtime() + (11*1000));
+        chronometer.setCountDown(true);
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                time++;
+                if (time == 10){
+                    chronometer.stop();
+                }
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.start();
+            }
+        });
 
     }
 
