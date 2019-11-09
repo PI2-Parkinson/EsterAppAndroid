a1f037e2d0db9b0737a860b409276f24d3aa7501
Cecilia <ceciliabessa@hotmail.com>
2019-11-09 15:40:48 -0300

    Creating activity for exercise color

---
+++ app/src/main/java/com/pidois/ester/Controller/ExerciseColorActivity.java
@@ -0,0 +1,16 @@
+package com.pidois.ester.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pidois.ester.R;
+
+public class ExerciseColorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_color);
+    }
+}
